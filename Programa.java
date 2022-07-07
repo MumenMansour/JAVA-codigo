257ccf8a141516697a8c6b6b9e31ff3f76b85c52
Mumen Mansour <103611001+MumenMansour@users.noreply.github.com>
2022-07-07 14:30:30 -0300

    Initial commit

---
+++ Programa.java
@@ -0,0 +1,12 @@
+// Language: java
+
+//class publica
+public class Programa {
+    public static void main(String[] args) {
+        //declaração de variáveis
+        //saida padrao do msDOS
+        System.out.println("Hello World");
+    }
+}
+
+
